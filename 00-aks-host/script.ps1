4b80c65f1d35704cbda0311dcff518a0525dafdd
Andreas Botsikas <andreasbotsikas@users.noreply.github.com>
2023-03-16 07:59:15 +0200

    Update video reference

--- 00-aks-host/script.ps1
+++ 00-aks-host/script.ps1
@@ -1,3 +1,6 @@
+# Variables used in this script, defined in ../env.ps1
+# $subscriptionId, $tenenantId, $resourceGroup
+
 # Sign in to Azure
 Connect-AzAccount -Tenant $tenenantId
 Set-AzContext -Subscription $subscriptionId
@@ -16,6 +19,8 @@ Get-AzResourceProvider -ProviderNamespace Microsoft.KubernetesConfiguration
 
 # Initialize node
 Initialize-AksHciNode
+
+# Create folders in V: drive
 New-Item -Path "V:\" -Name "AKS-HCI" -ItemType "directory" -Force
 New-Item -Path "V:\AKS-HCI\" -Name "Images" -ItemType "directory" -Force
 New-Item -Path "V:\AKS-HCI\" -Name "WorkingDir" -ItemType "directory" -Force
@@ -23,13 +28,14 @@ New-Item -Path "V:\AKS-HCI\" -Name "Config" -ItemType "directory" -Force
 
 # Create network
 $vnet = New-AksHciNetworkSetting -name "mgmtvnet" -vSwitchName "InternalNAT" -gateway "192.168.0.1" -dnsservers "192.168.0.1" `
-    -ipaddressprefix "192.168.0.0/24" -k8snodeippoolstart "192.168.0.3" -k8snodeippoolend "192.168.0.149" `
+    -ipaddressprefix "192.168.0.0/16" -k8snodeippoolstart "192.168.0.3" -k8snodeippoolend "192.168.0.149" `
     -vipPoolStart "192.168.0.150" -vipPoolEnd "192.168.0.250"
 
-# Install aks host (use specific version if you're planning to deploy Arc Resource Bridge)
+# Configure aks host (use specific version if you're planning to deploy Arc Resource Bridge)
 Set-AksHciConfig -vnet $vnet -imageDir "V:\AKS-HCI\Images" -workingDir "V:\AKS-HCI\WorkingDir" `
    -cloudConfigLocation "V:\AKS-HCI\Config" -Verbose -version "1.0.13.10907"
 
 Set-AksHciRegistration -SubscriptionId $subscriptionId -ResourceGroupName $resourceGroup
 
+# Install AKS host
 Install-AksHci
